8508bf791ecad5c687d20421176612a7b06913fd
Trupti Parmar <ridhiparmar07@gmail.com>
2026-04-12 17:42:48 +0530

    updated api base url for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Added /api to base for cleaner calls
+  baseURL: "import.meta.env.VITE_API_URL", // Added /api to base for cleaner calls
 });
 
 // Automatically attach token to every request
